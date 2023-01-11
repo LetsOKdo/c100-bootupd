59cdd1690528cc6ce73850bfcd6a6e82a14e6086
ZHANG Yuntian <yt@radxa.com>
2023-01-11 15:16:05 +0800

    Add build-data

---
+++ data/u-boot-preboot.cmd
@@ -0,0 +1,20 @@
+if test ! -e mmc 1:1 /etc/systemd/system/nv_boot_control.service
+then
+    echo [C100] Installing nv_boot_control.conf
+    load mmc 0:1 ${scriptaddr} /etc/nv_boot_control.conf
+    save mmc 1:1 ${scriptaddr} /usr/local/share/nv_boot_control.conf ${filesize}
+    echo [C100] Installing nv_boot_control.service
+    load mmc 0:1 ${scriptaddr} /etc/systemd/system/nv_boot_control.service
+    save mmc 1:1 ${scriptaddr} /etc/systemd/system/nv_boot_control.service ${filesize}
+    ln mmc 1:1 ../nv_boot_control.service /etc/systemd/system/nv-oem-config.target.wants/nv_boot_control.service
+fi
+
+if test ! -e mmc 1:1 /boot/tegra210-p3448-common-sdmmc3.dtbo
+then
+    echo [C100] Installing tegra210-p3448-common-sdmmc3.dtbo
+    load mmc 0:1 ${scriptaddr} /boot/tegra210-p3448-common-sdmmc3.dtbo
+    save mmc 1:1 ${scriptaddr} /boot/tegra210-p3448-common-sdmmc3.dtbo ${filesize}
+    echo [C100] Installing extlinux-mmcblk1p1.conf
+    load mmc 0:1 ${scriptaddr} /boot/extlinux/extlinux-mmcblk1p1.conf
+    save mmc 1:1 ${scriptaddr} /boot/extlinux/extlinux.conf ${filesize}
+fi
